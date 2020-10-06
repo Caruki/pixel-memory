950946733311faee9e278ea8cf998cabe29afffd
Caruki <caruki91@posteo.de>
2020-10-06 10:38:36 +0200

    Generate basic card and card-grid components

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Card } from '../../models/card';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+  @Input() card: Card;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onCardClick(card: Card): void {
+    console.log(card.title);
+  }
+}

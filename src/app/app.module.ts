169ca87f096a12b0b9d48ac4639df07bb7322aa6
Caruki <caruki91@posteo.de>
2020-10-07 19:21:43 +0200

    Add header and welcome components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import { CardGridComponent } from './components/card-grid/card-grid.component';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { PlayerOverviewComponent } from './components/player-overview/player-overview.component';
+import { HeaderComponent } from './components/header/header.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent, CardGridComponent, PlayerOverviewComponent],
+  declarations: [AppComponent, CardComponent, CardGridComponent, PlayerOverviewComponent, HeaderComponent, WelcomeComponent],
   imports: [BrowserModule, AppRoutingModule, CommonModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]

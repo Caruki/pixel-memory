83b17c8401cb62e6d9f51d66846f06d5bb2eefe7
Caruki <caruki91@posteo.de>
2020-10-08 12:46:57 +0200

    Create welcome form for player count and overall before game mechanics

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+import { BoardService } from 'src/app/services/board.service';
+import { PlayerService } from 'src/app/services/player.service';
 
 @Component({
   selector: 'app-welcome',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent implements OnInit {
+  constructor(
+    public playerService: PlayerService,
+    private boardService: BoardService
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  playerCount: number;
+  chosenName: string;
+  showGame: boolean = false;
+
+  setName(value: object) {}
+
+  onSubmit() {
+    this.playerService.initPlayers(this.playerCount);
+    console.log(this.playerService.PlayerList);
   }
 
+  startGame() {
+    this.playerService.PlayerList[0].active = true;
+    this.boardService.toggleShow();
+    console.log(this.playerService.PlayerList);
+  }
 }

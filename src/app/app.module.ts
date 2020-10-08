1c0268c3217ac134cb9c01825192c248e7e6aa06
Caruki <caruki91@posteo.de>
2020-10-08 17:02:07 +0200

    Create basic show game after welcome finished mechanic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 import { CardGridComponent } from './components/card-grid/card-grid.component';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { PlayerOverviewComponent } from './components/player-overview/player-overview.component';
 import { HeaderComponent } from './components/header/header.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { PlayerFormComponent } from './components/player-form/player-form.component';
+import { PlayerDetailComponent } from './components/player-detail/player-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent, CardGridComponent, PlayerOverviewComponent, HeaderComponent, WelcomeComponent, PlayerFormComponent],
-  imports: [BrowserModule, AppRoutingModule, CommonModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    CardComponent,
+    CardGridComponent,
+    PlayerOverviewComponent,
+    HeaderComponent,
+    WelcomeComponent,
+    PlayerFormComponent,
+    PlayerDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule,
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })

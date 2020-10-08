395f000489e2dd98dbae65e8ddb765df3f2a3d16
Caruki <caruki91@posteo.de>
2020-10-08 12:46:14 +0200

    Create player form that allows and tracks input of player names

--- src/app/components/player-form/player-form.component.ts
+++ src/app/components/player-form/player-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { PlayerService } from 'src/app/services/player.service';
 
 @Component({
   selector: 'app-player-form',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player-form.component.scss']
 })
 export class PlayerFormComponent implements OnInit {
+  constructor(private playerService: PlayerService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  playerName: string;
+
+  @Input() title: number;
+
+  onNameInput() {
+    switch (this.title) {
+      case 0:
+        this.playerService.PlayerList[0].name = this.playerName;
+        break;
+      case 1:
+        this.playerService.PlayerList[1].name = this.playerName;
+        break;
+      case 2:
+        this.playerService.PlayerList[2].name = this.playerName;
+        break;
+      case 3:
+        this.playerService.PlayerList[3].name = this.playerName;
+        break;
+      case 4:
+        this.playerService.PlayerList[4].name = this.playerName;
+        break;
+      default:
+    }
   }
-
 }
